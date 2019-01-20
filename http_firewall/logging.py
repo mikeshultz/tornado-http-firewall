5128fd59dee7f7419a1bb68f224288f3125faab3
Mike Shultz <shultzm@gmail.com>
2019-01-20 14:58:36 -0700

    fixed logging, and got ACL checks working

--- http_firewall/logging.py
+++ http_firewall/logging.py
@@ -1,6 +1,8 @@
 import logging
 
 parent_logger = logging.getLogger()
+console_handler = logging.StreamHandler()
+parent_logger.addHandler(console_handler)
 
 
 def get_logger(name):
@@ -8,9 +10,12 @@ def get_logger(name):
     return parent_logger.getChild(name)
 
 
-def set_debug_logging():
+def set_debug_logging(logger=None):
     """ Set debug level loggin """
     parent_logger.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.DEBUG)
+    if logger:
+        logger.setLevel(logging.DEBUG)
 
 
 def logging_shutdown():
--- http_firewall/main.py
+++ http_firewall/main.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 from typing import Type, List, Tuple
 from . import __doc__ as http_firewall_description
 from .logging import get_logger, set_debug_logging
+from .acl import load_acl_config, check_allowed
 
 log = get_logger(__name__)
 
@@ -14,6 +15,13 @@ HTTP_HEADER_PATTERN = r'^(?P<name>[\w\d\-]+): (?P<value>[\w\d\s]+)\r\n$'
 BLOCK_HEADERS = [
     'Transfer-Encoding'
 ]
+
+
+def strip_get_params(path: str) -> str:
+    """ Strip get params from a path """
+    if '?' in path:
+        return path.split('?')[0]
+    return path
 
 
 def build_backend_url(root_url: str, path: str, arguments: dict) -> str:
@@ -24,7 +32,7 @@ def build_backend_url(root_url: str, path: str, arguments: dict) -> str:
     # Strip the param string from the path if it's there.  Unclear why it's
     # not always one way or the other
     if '?' in path and len(arguments) > 0:
-        path = path.split('?')[0]
+        path = strip_get_params(path)
 
     for k, vs in arguments.items():
         for val in vs:
@@ -50,6 +58,10 @@ class ForwardHandler(tornado.web.RequestHandler):
         log.debug("get({}, {})".format(args, kwargs))
         # get path
         path = self.request.uri
+
+        # Check the request against the ACL
+        if not check_allowed(strip_get_params(path), self.request.remote_ip):
+            raise tornado.web.HTTPError(403)
 
         log.debug("request path: {}".format(path))
 
@@ -119,16 +131,29 @@ def make_app() -> tornado.web.Application:
 def main(args=None):
     """ Main app entry point """
 
-    # TODO: Remove below
-    set_debug_logging()
+    log.info("Starting up http_filrewall")
 
     parser = ArgumentParser(description=http_firewall_description)
     parser.add_argument('-a', '--address', type=str, default='127.0.0.1',
                         help='Port number to listen on')
     parser.add_argument('-p', '--port', type=int, default=8080,
                         help='Port number to listen on')
+    parser.add_argument('-c', '--config', type=str,
+                        default='/etc/http_filrewall.yml',
+                        help='The ACL config YAML file')
+    parser.add_argument('-d', '--debug', action="store_true",
+                        default=False,
+                        help='Show debug messages')
     args = parser.parse_args(args or sys.argv[1:])
 
+    if args.debug is True:
+        set_debug_logging(log)
+
+    log.info("Loading ACL configuration...")
+
+    load_acl_config(args.config)
+
+    log.info("Starting HTTP server...")
     app = make_app()
     app.listen(args.port, address=args.address)
     tornado.ioloop.IOLoop.current().start()
