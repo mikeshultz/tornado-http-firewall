a7c99d25e7829b0bc66bbbb9f0a255d0cef74aa0
Mike Shultz <shultzm@gmail.com>
2019-01-20 17:12:36 -0700

    more ACL tests

--- tests/test_acl.py
+++ tests/test_acl.py
@@ -1,4 +1,10 @@
-from http_firewall.acl import load_config_file
+from http_firewall.acl import (
+    ALLOW,
+    load_config_file,
+    build_acl,
+    load_acl_config,
+    check_allowed,
+)
 from .const import ACL_CONFIG_1
 
 
@@ -21,3 +27,65 @@ def test_acl_load_config(temp_dir):
         assert isinstance(loaded['roles']['127.0.0.1'], list)
         assert len(loaded['roles']['public']) == 2
         assert len(loaded['roles']['127.0.0.1']) == 1
+
+
+def test_acl_build_acl(temp_dir):
+    """ test build_acl """
+
+    with temp_dir() as _tmpdir:
+        config_file = _tmpdir.joinpath('test.ini')
+
+        with config_file.open('w') as _file:
+            _file.write(ACL_CONFIG_1)
+
+        # Write our test config
+        loaded = load_config_file(str(config_file))
+        new_acl = build_acl(loaded)
+
+        assert new_acl is not None
+        assert new_acl.check('public', '/api/v0/get', ALLOW)
+        assert new_acl.check('public', '/api/v0/pin/ls', ALLOW)
+        assert new_acl.check('127.0.0.1', '/api/v0/ping', ALLOW)
+        assert not new_acl.check('127.0.0.1', '/api/v0/get', ALLOW)
+        assert not new_acl.check('127.0.0.1', '/api/v0/pin/ls', ALLOW)
+        assert not new_acl.check('public', '/api/v0/ping', ALLOW)
+
+
+def test_acl_load_acl_config(temp_dir):
+    """ test build_acl """
+
+    with temp_dir() as _tmpdir:
+        config_file = _tmpdir.joinpath('test.ini')
+
+        with config_file.open('w') as _file:
+            _file.write(ACL_CONFIG_1)
+
+        # Write our test config
+        new_acl = load_acl_config(str(config_file))
+
+        assert new_acl is not None
+        assert new_acl.check('public', '/api/v0/get', ALLOW)
+        assert new_acl.check('public', '/api/v0/pin/ls', ALLOW)
+        assert new_acl.check('127.0.0.1', '/api/v0/ping', ALLOW)
+        assert not new_acl.check('127.0.0.1', '/api/v0/get', ALLOW)
+        assert not new_acl.check('127.0.0.1', '/api/v0/pin/ls', ALLOW)
+        assert not new_acl.check('public', '/api/v0/ping', ALLOW)
+
+
+def test_acl_check_allowed(temp_dir):
+    """ test build_acl """
+
+    with temp_dir() as _tmpdir:
+        config_file = _tmpdir.joinpath('test.ini')
+
+        with config_file.open('w') as _file:
+            _file.write(ACL_CONFIG_1)
+
+        # Write our test config
+        new_acl = load_acl_config(str(config_file))
+
+        assert check_allowed('/api/v0/get')
+        assert check_allowed('/api/v0/pin/ls')
+        assert check_allowed('/api/v0/pin/ls', '127.0.0.1')
+        assert check_allowed('/api/v0/ping', '127.0.0.1')
+        assert not check_allowed('/api/v0/ping')
